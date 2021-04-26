fd54e61749bd6b1985e8c83f359767d3c00a8213
john <tierney-john@hotmail.com>
2021-04-26 17:46:07 +0800

    更新 maven 依赖结构

--- acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsProperties.java
+++ acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsProperties.java
@@ -1,8 +1,13 @@
 package org.acgs.autoconfigure.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
+ * acgs 配置文件
+ *
  * @Author John@acgs-org
  * @Date 2021/4/26
  */
 public class AcgsCmsProperties {
+
 }
