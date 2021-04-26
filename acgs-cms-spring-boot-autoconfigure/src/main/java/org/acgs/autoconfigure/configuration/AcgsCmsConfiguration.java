d35089f15a77be66fb6319feb3b9d7cdb170a907
john <tierney-john@hotmail.com>
2021-04-26 18:24:43 +0800

    新增项目配置文件

--- acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsConfiguration.java
+++ acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsConfiguration.java
@@ -1,8 +1,42 @@
 package org.acgs.autoconfigure.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
 /**
+ * acgs cms 配置文件
+ *
  * @Author John@acgs-org
  * @Date 2021/4/26
  */
+@Configuration(proxyBeanMethods = false)
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties(AcgsCmsProperties.class)
 public class AcgsCmsConfiguration {
+
+    @Autowired
+    private AcgsCmsProperties properties;
+
+    /**
+     * @return jwt bean
+     */
+//    @Bean
+//    public DoubleJWT jwt() {
+//        String secret = properties.getTokenSecret();
+//        Long accessExpire = properties.getTokenAccessExpire();
+//        Long refreshExpire = properties.getTokenRefreshExpire();
+//        if (accessExpire == null) {
+//            // 一个小时
+//            accessExpire = 60 * 60L;
+//        }
+//        if (refreshExpire == null) {
+//            // 一周
+//            refreshExpire = 60 * 60 * 24 * 7L;
+//        }
+//        return new DoubleJWT(secret, accessExpire, refreshExpire);
+//    }
+
 }
--- acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsProperties.java
+++ acgs-cms-spring-boot-autoconfigure/src/main/java/org/acgs/autoconfigure/configuration/AcgsCmsProperties.java
@@ -8,6 +8,48 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @Author John@acgs-org
  * @Date 2021/4/26
  */
+@ConfigurationProperties("acgs.cms")
 public class AcgsCmsProperties {
 
+    private static final String[] DEFAULT_EXCLUDE_METHODS = new String[]{"OPTIONS"};
+
+    private String[] excludeMethods = DEFAULT_EXCLUDE_METHODS;
+
+    private String tokenSecret = "";
+
+    private Long tokenAccessExpire = 3600L;
+
+    private Long tokenRefreshExpire = 2592000L;
+
+    public String getTokenSecret() {
+        return tokenSecret;
+    }
+
+    public void setTokenSecret(String tokenSecret) {
+        this.tokenSecret = tokenSecret;
+    }
+
+    public Long getTokenAccessExpire() {
+        return tokenAccessExpire;
+    }
+
+    public void setTokenAccessExpire(Long tokenAccessExpire) {
+        this.tokenAccessExpire = tokenAccessExpire;
+    }
+
+    public Long getTokenRefreshExpire() {
+        return tokenRefreshExpire;
+    }
+
+    public void setTokenRefreshExpire(Long tokenRefreshExpire) {
+        this.tokenRefreshExpire = tokenRefreshExpire;
+    }
+
+    public String[] getExcludeMethods() {
+        return excludeMethods;
+    }
+
+    public void setExcludeMethods(String[] excludeMethods) {
+        this.excludeMethods = excludeMethods;
+    }
 }
